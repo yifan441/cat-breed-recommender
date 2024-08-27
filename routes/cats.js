ec0664f7a23f15323ef7db2c52c3f4b100cca1d3
Yifan Tang <yifant@g.ucla.edu>
2024-08-26 21:38:56 -0700

    Added error handling easter egg

--- routes/cats.js
+++ routes/cats.js
@@ -3,30 +3,31 @@ const router = express.Router();
 
 // Cat breed form
 router.get("/", (req, res) => {
-  res.render("cats/index", {test: "this is how you send data to .ejs files"})
+  res.render("cats/index")
 })
 
 // Find suitable cat breeds
 router.get("/results", async (req, res) => {
   const queryString = req._parsedUrl.search; 
-  // const testingString = "?name=siamese"; 
   const url = "https://api.api-ninjas.com/v1/cats" + queryString;
-  
+  // const TEST_HTTP_CODES_URL = "https://httpstat.us/500";
+
   try{
-    const results = await fetch(url, {
+    const response = await fetch(url, {
       method: "GET",
       headers: {
-        'X-Api-Key': process.env.API_KEY
+        'X-Api-Key': process.env.API_KEY,
       }
     })
-    if(!results.ok){
-      throw new Error(`Response status: ${results.status}`);
+    if(!response.ok){
+      throw response.status;
     }
-    const catBreeds = await results.json();
-    console.log(catBreeds);
+    const catBreeds = await response.json();
     res.render("cats/results.ejs", {data: catBreeds});
-  } catch(error){
-    console.error(`something went wrong lmao\n=> ${error.message}`)
+  } 
+  catch(errorStatus){
+    console.error(`an error occured lmao, status code: ${errorStatus}`);
+    res.render("partials/error.ejs", {errorStatusCode: errorStatus});
   }
 
 })
